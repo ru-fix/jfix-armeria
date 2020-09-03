6cff25b391c724e1a7038754fda64ab567aea329
Timur <t.kasatkin.o@gmail.com>
2020-09-03 18:55:56 +0300

    Profiling decorator improved and fixed: (#9)
    - now http_connect metric writes host/port tags
    - connect_refused error detection fixed (when it is placed before retrying decorator

--- jfix-armeria-commons/src/main/kotlin/ru/fix/armeria/commons/RetryRules.kt
+++ jfix-armeria-commons/src/main/kotlin/ru/fix/armeria/commons/RetryRules.kt
@@ -2,9 +2,9 @@ package ru.fix.armeria.commons
 
 import com.linecorp.armeria.client.retry.RetryRule
 import com.linecorp.armeria.common.HttpStatus
-import java.net.ConnectException
 
-val On503AndConnectExceptionRetryRule: RetryRule = RetryRule.onStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    .orElse(RetryRule.onException { _, thr ->
-        thr.unwrapUnprocessedExceptionIfNecessary() is ConnectException
-    })
+val On503AndUnprocessedRetryRule: RetryRule = RetryRule
+    .builder()
+    .onStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    .onUnprocessed()
+    .thenBackoff()
